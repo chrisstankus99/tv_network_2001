8b2caab9eafec1cb84614c8a08d7143132243ac1
Chris Stankus <chrisstankus99@gmail.com>
2020-02-11 09:47:50 -0700

    Add character class, show class, and tests for both

---
+++ lib/show.rb
@@ -0,0 +1,28 @@
+class Show
+
+  attr_reader :name, :creator, :characters
+  def initialize(name, creator, characters)
+    @name = name
+    @creator = creator
+    @characters = characters
+  end
+
+  def total_salary
+    @characters.sum do |character|
+      character.salary
+    end
+  end
+
+  def highest_paid_actor
+    highest_paid = @characters.max_by {|character| character.salary}
+    highest_paid.actor
+  end
+
+  def actors
+    array_of_actors = []
+    @characters.each do |character|
+      array_of_actors << character.actor
+    end
+    array_of_actors
+  end
+end
